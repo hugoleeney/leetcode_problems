23c864922d634b6d2dc5af6edb2d7cf1c47320e0
hugoleeney <hugo@narnianursery.ie>
2020-11-26 12:26:55 +0000

    utils update

--- utils.py
+++ utils.py
@@ -26,3 +26,59 @@ def list_to_binary_tree(a):
             n.left = TreeNode(a[left_idx])
             q.append((n.left, left_idx))
     return root
+
+
+def arr_assign(arr, key, val):
+    try:
+        arr[key] = val
+        return
+    except IndexError:
+        # Do not extend the array for negative indices
+        # That is ridiculously counterintuitive
+        assert key >= 0
+        arr.extend(((key + 1) - len(arr)) * [None])
+        arr[key] = val
+        return
+
+
+def binary_tree_to_list(node):
+    stack = [(node, 0)]
+    result = []
+    while stack:
+        n, pos = stack.pop()
+        arr_assign(result, pos, n.val)
+        if n.left:
+            stack.append((n.left, pos*2 +1))
+        if n.right:
+            stack.append((n.right, pos*2 +2))
+    return result
+
+
+if __name__ == "__main__":
+    tc = [1,2,3,4,5,6,7]
+    bt = list_to_binary_tree(tc)
+    res = binary_tree_to_list(bt)
+    assert tc == res
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def list_to_linked_list(l):
+    head = ListNode(l[0])
+    cur = head
+    for n in l[1:]:
+        cur.next = ListNode(n)
+        cur = cur.next
+    return head
+
+
+def linked_list_to_list(head):
+    r = [head.val]
+    while head.next:
+        r.append(head.next.val)
+        head = head.next
+    return r
